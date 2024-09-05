7c1d3df50425224fa1d938b60502d2cab1349c8f
Hillel Arnold <hillel.arnold@gmail.com>
2024-09-05 11:09:50 -0400

    implement management command in views and templates

--- exporttool/views.py
+++ exporttool/views.py
@@ -7,6 +7,8 @@ from .models import Entity, Column
 from subprocess import run
 from django.views.decorators.csrf import csrf_exempt
 
+from .management.commands.export import Command
+
 # Create your views here.
 
 @csrf_exempt
@@ -29,14 +31,25 @@ def export_data(request):
         checked_columns = data.get('checkedColumns', [])
         filter_value = data.get('filter', '')
         format_value = data.get('format', '')
+        location = data.get('location')
 
         # Process the checked columns and filter value as needed
         print("Checked Columns:", checked_columns)
         print("Filter Value:", filter_value)
         print("Format value:", format_value)
+        print("Export location:", location)
+
+        entity_name = [checked_columns[0].split("|")[0]]
+        clean_columns = ",".join(entity_name + [i.split("|")[-1] for i in checked_columns])
 
         # Call the exportScript.py script with the data
-        run(["python", "exportScript.py", *checked_columns, filter_value, format_value])
+        # run(["python", "exportScript.py", *checked_columns, filter_value, format_value])
+        Command().handle(
+            format=format_value,
+            filter=filter_value,
+            inputs=clean_columns,
+            location=location,
+        )
 
         # Return a success JSON response
         return JsonResponse({'success': True})
