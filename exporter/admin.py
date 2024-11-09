ac939adab8c3e2d311c526eae57e0f8e4122fae4
Hillel Arnold <hillel.arnold@gmail.com>
2024-11-08 21:34:00 -0500

    add S3Config admin

--- exporter/admin.py
+++ exporter/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Column, Entity, FluxxConfig, User
+from .models import Column, Entity, FluxxConfig, S3Config, User
 
 admin.site.register(Entity)
 admin.site.register(Column)
 admin.site.register(User)
 admin.site.register(FluxxConfig)
+admin.site.register(S3Config)
