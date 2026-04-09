bb9e471f1171233e9997191f16ed456396e44363
Patrick <pjg250@gmail.com>
2026-04-09 15:06:34 -0400

    Update start.py
    Updates start.py to accept a signal to kill the application from the command line.

--- start.py
+++ start.py
@@ -1,12 +1,16 @@
-# This script starts the compiled Fluxx Exporter application.
-# In development, entrypoint.sh is targeted instead.
-
 import os
+import signal
+import sys
 import webbrowser
 
 from django.core.management import execute_from_command_line
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fluxx_exporter.settings")
+
+
+def handle_shutdown(signum, frame):
+    print(f"Received signal {signum}. Shutting down Fluxx Exporter...")
+    sys.exit(0)
 
 
 def migrate_db():
@@ -18,6 +22,8 @@ def run_server():
 
 
 if __name__ == "__main__":
+    signal.signal(signal.SIGTERM, handle_shutdown)
+    signal.signal(signal.SIGINT, handle_shutdown)
     print("Starting Fluxx Exporter")
     migrate_db()
     webbrowser.open("http://localhost:8000")
