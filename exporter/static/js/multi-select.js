04db24aca5e5adcf62e3534809521437d8eb7215
Hillel Arnold <hillel.arnold@gmail.com>
2026-05-19 14:43:59 -0400

    support use of shift to select multiple checkboxes

---
+++ exporter/static/js/multi-select.js
@@ -0,0 +1,31 @@
+let multiSelectCheckboxes = document.querySelectorAll('.multi-select')
+var lastChecked = null;
+
+multiSelectCheckboxes.forEach(element => {
+    element.addEventListener('click', function(e) {
+        if (!lastChecked) {
+            lastChecked = this;
+            return;
+        }
+
+        if (e.shiftKey) {
+            const siblingCheckboxes = [...this.parentElement.parentElement.children].filter(c => c.classList.contains('input-group'))
+
+            const from = siblingCheckboxes.indexOf(this.parentElement)
+            const to = siblingCheckboxes.indexOf(lastChecked.parentElement)
+
+            const start = Math.min(from, to);
+            const end = Math.max(from, to) + 1;
+
+            const toControl = siblingCheckboxes.slice(start, end)
+
+            toControl.forEach(el => {
+                let checkbox = [...el.children].find(c => c.classList.contains('checkbox'))
+                checkbox.checked = lastChecked.checked
+            })
+        }
+
+        lastChecked = this;
+
+    })
+})
